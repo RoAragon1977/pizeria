4da4b495c02f6a0afa83b1ef9b0060970c8d09be
Rodrigo Aragon <rodrigoaragon1977@gmail.com>
2022-11-19 13:12:07 -0300

    v6

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,61 @@
-let num = 8;
-if (num>=10) {
-    console.log("num es mayor o igual a 10")
+/*
+let num = 16;
+if (num>10) {
+    console.log("num es mayor a 10")
+} else if (num<10) {
+    console.log("num es menor a 10")
 } else {
-    console.log("num es menor a 10 o es 0")
+    console.log("num es igual a 10")
 }
+if (num<=20 && num>10) {
+    console.log("el numero esta entre 10 y 20")
+} else if (num>20) {
+    console.log("el numero es mayor a 20")
+} else {
+    console.log("el numero es menor que 10")
+}*/
+
+/*let edad = 18;
+let tutor = false;
+if(edad >= 18) {
+    console.log("puede pasar");
+}else if(tutor===true) {
+    console.log("puede pasar");
+}else {
+    console.log("no podes pasar");
+}*/
+
+//ejercicio 6
+
+/*let num1 = prompt ("Ingresa un numero")
+let num2 = prompt ("Ingresa otro numero")
+let res = parseInt(num1) + parseInt(num2) 
+console.log ("La suma es igual a",res)*/
+
+// ejercicio 7
+
+/*let num1 = prompt ("Ingresa un numero")
+let num2 = prompt ("Ingresa otro numero")
+let num3 = prompt ("Ingresa otro numero")
+if(num1>num2 && num1>num3) {
+    console.log(num1,"es el mayor")
+}else if(num2>num3) {
+    console.log(num2,"es el mayor")
+}else {
+    console.log(num3,"es el mayor")
+}*/
+
+//ejercicio 8
+
+/*let num = prompt("ingrese un numero")
+let res = parseInt(num) % 2
+if(res == 0) {
+    console.log("El numero es divisible por 2")
+}else {
+    console.log("El numero no es divisible por 2")
+}*/
+
+//ejercicio 9
+
+let text = prompt("escribe un texto breve")
+for()
